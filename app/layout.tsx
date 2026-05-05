c1be947a933adff0bf6ca0690ae06503f4a03277
topnotchsw-roof <djones@topnotchsouthwestroofing.com>
2026-05-05 16:54:01 -0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,8 +8,14 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
-      <body>{children}</body>
-    </html>
-  );
+  <html lang="en">
+    <head>
+      <link
+        rel="stylesheet"
+        href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css"
+      />
+    </head>
+    <body>{children}</body>
+  </html>
+);
 }
