8a668402adb57a31c5d281b45a9ab8fc5dd04a72
topnotchsw-roof <djones@topnotchsouthwestroofing.com>
2026-05-05 17:24:02 -0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -277,7 +277,9 @@ export default function Home() {
 
   </div>
 </section>
-      <footer><strong>Top Notch Southwest Roofing</strong><br />Licensed • Insured • Bonded • ROC #339754 • 602-686-5922 • Phoenix Metro Roofing Contractor</footer>
+      <footer>
+  <strong>Top Notch Southwest Roofing</strong><br />
+  38617 N 7th St, Phoenix AZ 85086<br />Licensed • Insured • Bonded • ROC #339754 • 602-686-5922 • Phoenix Metro Roofing Contractor</footer>
     
    
     </main>
